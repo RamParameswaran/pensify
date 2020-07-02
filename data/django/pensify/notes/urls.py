3a059eea27aff9a78fe00d3656abe43b07952049
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2020-07-03 09:48:52 +1000

    Project structure cleanup - Removed django from project dir - Removed /requirements becuase Python is not a dependency anymore - Removed /bin and /etc because we won't be doing a bare-metal deployment

--- data/django/pensify/notes/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import routers
-
-from .views import NoteViewSet
-
-app_name = "notes"
-
-router = routers.SimpleRouter()
-router.register(r'notes', NoteViewSet, 'notes')
-urlpatterns = router.urls
