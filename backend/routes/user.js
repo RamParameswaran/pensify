df29b4ddd9d3c03ca60216f917a6d9e2d4a27e35
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2020-07-04 21:01:03 +1000

    Backend - created social auth routes (for Google and Facebook)

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,109 +1,164 @@
-const express = require("express");
-const router = express.Router();
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const express = require('express')
+const router = express.Router()
+const jwt = require('jsonwebtoken')
+const { OAuth2Client } = require('google-auth-library')
+var axios = require('axios')
 
-const User = require("../models/user");
+const checkAuth = require('../middleware/check-auth')
 
-router.post("/signup", (req, res, next) => {
-  User.find({ email: req.body.email })
-    .exec()
-    .then(user => {
-      if (user.length >= 1) {
-        return res.status(409).json({
-          message: "Mail exists"
-        });
-      } else {
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-          if (err) {
-            return res.status(500).json({
-              error: err
-            });
-          } else {
-            const user = new User({
-              _id: new mongoose.Types.ObjectId(),
-              email: req.body.email,
-              password: hash
-            });
-            user
-              .save()
-              .then(result => {
-                console.log(result);
-                res.status(201).json({
-                  message: "User created"
-                });
-              })
-              .catch(err => {
-                console.log(err);
-                res.status(500).json({
-                  error: err
-                });
-              });
-          }
-        });
-      }
-    });
-});
+const User = require('../models/user')
 
-router.post("/login", (req, res, next) => {
-  User.find({ email: req.body.email })
-    .exec()
-    .then(user => {
-      if (user.length < 1) {
-        return res.status(401).json({
-          message: "Auth failed"
-        });
-      }
-      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-        if (err) {
-          return res.status(401).json({
-            message: "Auth failed"
-          });
+const googleClient = new OAuth2Client(process.env.GOOGLE_OAUTH_CLIENTID)
+
+function createAuthToken(email, id) {
+    return jwt.sign(
+        {
+            email: email,
+            userId: id,
+        },
+        process.env.JWT_KEY,
+        {
+            expiresIn: '6h',
         }
-        if (result) {
-          const token = jwt.sign(
-            {
-              email: user[0].email,
-              userId: user[0]._id
-            },
-            process.env.JWT_KEY,
-            {
-                expiresIn: "1h"
-            }
-          );
-          return res.status(200).json({
-            message: "Auth successful",
-            token: token
-          });
-        }
-        res.status(401).json({
-          message: "Auth failed"
-        });
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        error: err
-      });
-    });
-});
+    )
+}
 
-router.delete("/:userId", (req, res, next) => {
-  User.remove({ _id: req.params.userId })
-    .exec()
-    .then(result => {
-      res.status(200).json({
-        message: "User deleted"
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        error: err
-      });
-    });
-});
+router.post('/login-with-google', (req, res, next) => {
+    googleClient
+        .verifyIdToken({
+            idToken: req.body.idToken,
+            audience: process.env.GOOGLE_OAUTH_CLIENTID,
+        })
+        .then((payload) => {
+            // Search if a user with this email address exists yet:
+            User.findOne({ email: payload.getPayload().email })
+                .exec()
+                .then((existingUser) => {
+                    if (existingUser) {
+                        // GET EXISTING USER INSTANCE FROM DB
+                        // Sign in this existing user
+                        res.status(201).json({
+                            message: 'Login successful',
+                            token: createAuthToken(
+                                existingUser.email,
+                                existingUser._id
+                            ),
+                        })
+                    } else {
+                        // CREATE A NEW USER IN DB
+                        const user = new User({
+                            email: payload.getPayload().email,
+                            name: payload.getPayload().name,
+                            firstName: payload.getPayload().given_name,
+                            lastName: payload.getPayload().family_name,
+                            profilePicUrl: `https://graph.facebook.com/10218122206874683/picture?type=square`,
+                        })
+                        user.save()
+                            .then((result) => {
+                                // Sign in this new user
+                                res.status(201).json({
+                                    message: 'Signup successful',
+                                    token: createAuthToken(
+                                        result.email,
+                                        result._id
+                                    ),
+                                })
+                            })
+                            .catch((err) => {
+                                console.log(err)
+                                res.status(500).json({
+                                    error: err,
+                                })
+                            })
+                    }
+                })
+                .catch((err) => {
+                    console.log(err)
+                    res.status(500).json({
+                        error: err,
+                    })
+                })
+        })
+        .catch((err) => {
+            console.log(err)
+            res.status(401).json({
+                message: 'Auth failed',
+            })
+        })
+})
 
-module.exports = router;
+router.post('/login-with-facebook', (req, res, next) => {
+    axios({
+        url: `https://graph.facebook.com/me`,
+        method: 'get',
+        params: {
+            fields: 'id,name,first_name,last_name,email',
+            access_token: req.body.accessToken,
+        },
+    })
+        .then((payload) => {
+            console.log(payload)
+            // Search if a user with this email address exists yet:
+            User.findOne({ email: payload.data.email })
+                .exec()
+                .then((existingUser) => {
+                    if (existingUser) {
+                        console.log('Returning existing user')
+                        // GET EXISTING USER INSTANCE FROM DB
+                        // Sign in this existing user
+                        res.status(201).json({
+                            message: 'Login successful',
+                            token: createAuthToken(
+                                existingUser.email,
+                                existingUser._id
+                            ),
+                        })
+                    } else {
+                        console.log('Creating new user')
+                        // CREATE A NEW USER IN DB
+                        const user = new User({
+                            email: payload.data.email,
+                            name: payload.data.name,
+                            firstName: payload.data.given_name,
+                            lastName: payload.data.family_name,
+                            profilePicUrl: `https://graph.facebook.com/${payload.data.id}/picture?type=large`,
+                        })
+                        user.save()
+                            .then((result) => {
+                                // Sign in this new user
+                                res.status(201).json({
+                                    message: 'Signup successful',
+                                    token: createAuthToken(
+                                        result.email,
+                                        result._id
+                                    ),
+                                })
+                            })
+                            .catch((err) => {
+                                console.log(err)
+                                res.status(500).json({
+                                    error: err,
+                                })
+                            })
+                    }
+                })
+                .catch((err) => {
+                    console.log(err)
+                    res.status(500).json({
+                        error: err,
+                    })
+                })
+        })
+        .catch((err) => {
+            console.log(err)
+            res.status(401).json({
+                message: 'Auth failed',
+            })
+        })
+})
+
+router.get('/', checkAuth, (req, res, next) => {
+    res.status(200).json({ data: 'success!' })
+})
+
+module.exports = router
